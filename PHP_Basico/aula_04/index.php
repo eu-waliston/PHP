1f7cd9345614e2562c0d07169b7e135ab30fab30
walwizard <goneisgone@hotmail.com>
2023-02-09 09:52:27 -0300

    Aulas 04

--- PHP_Basico/aula_04/index.php
+++ PHP_Basico/aula_04/index.php
@@ -1,1 +1,20 @@
 <?php
+//tipos de varaiveis
+
+// number podem ser -> int,float etc
+$idade = 25;
+
+//string -> repressenta uma cadeia de caracteres
+$nome = "waliston";
+
+//boolean -> pode mser true ou false
+$namora = false;
+$result;
+if($namora === false) {
+    $result = "Não";
+} else {
+    $result = "SIM";
+}
+
+//no php para concatenar strings usamos o ponto . 
+print "nome ".$nome." idade ".$idade." namorando: ".$result;
