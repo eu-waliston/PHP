ddfbcdd7d8ee9fbc16f86bad25b58ce03d663481
walwizard <goneisgone@hotmail.com>
2023-02-09 10:29:30 -0300

    Aulas 08

---
+++ PHP_Basico/aula_08/index.php
@@ -0,0 +1,11 @@
+<?php
+#Operações matemáticas
+#que podem ser ->  / * - + %
+#Lembrando que podemos explicitar a ordem com ouso de parenteses ()
+#Caso contrario a odem sera como a já conhecida * -> / -> + -> -
+
+$numero1 = 20;
+$numero2 = 30;
+
+$aux = $numero1  * ($numero1 * 80 );
+echo $aux;
