1d4fb415ae3f72d17134408a7888ba923b7e1534
walwizard <goneisgone@hotmail.com>
2023-02-09 09:37:41 -0300

    Aulas 03

--- PHP_Basico/aula_02/index.php
+++ PHP_Basico/aula_02/index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 2</title>
-</head>
-<body>
-    <?php
-    
-    ?>
-</body>
-</html>
+<?php
+// echo $_SERVER['DOCUMENT_ROOT'];
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
