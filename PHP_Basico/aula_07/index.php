29a5453eb126264054ad7ada85dcafdcc96f7df0
walwizard <goneisgone@hotmail.com>
2023-02-09 10:18:20 -0300

    Aulas 07

---
+++ PHP_Basico/aula_07/index.php
@@ -0,0 +1,14 @@
+<?php
+#concatenação
+$nome = "Waliston";
+$idade = 25;
+$cidade = "Sacramento";
+$esstado = 'MG';
+
+#modo "normal"
+// print 'Ola meu nome é'.$nome;
+
+#modo simplificado
+print "Meu nome é $nome";
+
+echo "<div class=\"$estado\">Uma div aqui </div>";
