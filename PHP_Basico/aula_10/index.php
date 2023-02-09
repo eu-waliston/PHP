14819818fec7c6616ce987e326d46ff7c55ab03a
walwizard <goneisgone@hotmail.com>
2023-02-09 13:57:47 -0300

    Aulas 11

--- PHP_Basico/aula_10/index.php
+++ PHP_Basico/aula_10/index.php
@@ -5,4 +5,4 @@ $n2 = 8;
 
 //Um sinal de igual = significa 'recebe'
 //Existem varios tipos de opradores de comparação
-// > >= < <= != ===
+// > >= < <= != === !
---
+++ PHP_Basico/aula_11/index.php
@@ -0,0 +1,15 @@
+<?php
+#Operadores logicos
+//eles podem ser || && xor
+
+$var1 = "valor1";
+$var2 = "valor2";
+$var3 = "valor3";
+
+// if($var1 == $var2 && $var2 == $var3) {
+
+// }
+
+if($var1 == $var2 || $var2 == $var3) {
+    
+}
