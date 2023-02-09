2538687417f56bfa379b65f95e98fe0b60a0acf6
walwizard <goneisgone@hotmail.com>
2023-02-09 10:39:11 -0300

    Aulas 09

---
+++ PHP_Basico/aula_09/index.php
@@ -0,0 +1,10 @@
+<?php
+#condições
+$v1 = "Guilherme";
+$v2 = "Alex";
+
+if($v1 == $v2) {
+    echo '<div style="width: 50px; height: 50px; background-color: blue;">IGUAL</div>';
+} else {
+    echo '<div style="width: 50px; height: 50px; background-color: red;">DIFF</div>';
+} 
