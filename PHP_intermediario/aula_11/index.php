296080bac8a1d013a50e3d69fe11475c79a97221
walwizard <goneisgone@hotmail.com>
2023-02-10 12:05:11 -0300

    PHP Intermediário Aula 11 Sintaxe alternativa kk

---
+++ PHP_intermediario/aula_11/index.php
@@ -0,0 +1,18 @@
+<?php
+#Sintaxe alternativa
+
+$nome = "uai";
+
+if($nome == "uai"):
+    echo 'verdade';
+endif;
+
+echo '<br />';
+
+$contador = 0;
+while($contador < 10):
+    $contador++;
+    echo 'oi';
+    echo '<br />';
+endwhile;
+
