abc0c6327c7cf0e9531704105be9725ee3400e14
walwizard <goneisgone@hotmail.com>
2023-02-10 10:03:56 -0300

    PHP Intermediário Aula 03

---
+++ PHP_intermediario/aula_03/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Essa função fará o codigo dormir por X segundos
+
+sleep(1);
+echo "Olá1";
+
+$nome = "Walistons";
+
+
+if($nome == "Waliston") {
+    echo "udo certo";
+} else {
+    //A função DIE irá parar o codigo porem isso irá finalizar a aplicação junto kkk
+    die("Morreu mas passa bem");
+}
