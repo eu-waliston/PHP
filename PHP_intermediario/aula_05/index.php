7d6d0ffd4f07ceda54b70b47d3e707a0c666e527
walwizard <goneisgone@hotmail.com>
2023-02-10 10:36:38 -0300

    PHP Intermediário Aula 05

---
+++ PHP_intermediario/aula_05/index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    *** Trabalhando com data ***
+
+date_default_timezone_set("America/Sao_Paulo");
+
+$data = date("d/m/Y H:i:s", time()+(60 * 10));
+
+echo $data;
+
+*/
+$titulo_do_site = "Ayooo Esquimo";
+include('header.php');
+?>
+
+<h1>Conteudo da Home</h1>
+
+<?php
+include('footer.php');
+?>
