674e43e48dc3986baf083035b211bae67a0cbb7e
walwizard <goneisgone@hotmail.com>
2023-02-15 19:03:38 -0300

    Select, Inner Join e queries avançadas

--- banco_de_dados_e_PHP/aula_05/index.php
+++ banco_de_dados_e_PHP/aula_05/index.php
@@ -1,16 +1,57 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=modulo_8', '', '');
+$pdo = new PDO('mysql:host=localhost;dbname=modulo_8', 'root', '99583100');
 
-$id = 2;
+// $sql = $pdo->prepare("SELECT * FROM posts WHERE categoria_id = 2");
 
-// OR functiona copmop 'ou' 
-// AND functiona como 'e'
+// $sql->execute();
 
-//Slq injection pra prevenir use ? e passe parametros 
-//no formato array dentro do execute
-$sql = $pdo->prepare("DELETE FROM `clientes` WHERE id=?");
+// $info = $sql->fetchAll();
 
-if ($sql->execute([$id])) {
-    echo "cliente Deletado com sucesso :)";
-}
+// echo '<pre>';
+// print_r($info);
+// echo '</pre>';
+
+// foreach ($info as $key => $value) {
+//     echo 'Titulo ' . $value['titulo'];
+//     echo '<br />';
+//     echo 'Noticia ' . $value['conteudo'];
+//     echo '<hr />';
+
+// }
+
+// for ($i = 0; $i < count($info); $i++) {
+//     echo 'Titulo ' . $info[$i]['titulo'];
+//     echo '<br />';
+//     echo 'Noticia ' . $info[$i]['conteudo'];
+//     echo '<hr />';
+// }
+
+$sql = $pdo->prepare("SELECT * FROM categorias");
+
+$sql->execute();
+
+$info = $sql->fetchAll();
+
+// foreach ($info as $key => $value) {
+//     $ccategoria_id = $value['categoria_id'];
+//     echo 'Exibindo post de: ' . $value['nome'];
+//     echo '<br />';
+//     $sql = $pdo->prepare("SELECT * FROM posts WHERE categoria_id = categoria_id ");
+//     $sql->execute();
+//     $infoPosts = $sql->fetchAll();
+
+//     foreach ($infoPosts as $key => $value) {
+//         echo 'Titulo ' . $value['titulo'];
+//         echo '<br />';
+//         echo 'Noticia ' . $value['conteudo'];
+//         echo '<hr />';
+//     }
+// }
+
+$sql = $pdo->prepare("SELECT * FROM posts INNER JOIN  categorias ON posts.categoria_id = categorias.id ");
+$sql->execute();
+$info = $sql->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+print_r($info);
+echo '<pre />';
