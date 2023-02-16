af741902d4d0b2addc9cc377640c4d8ede0bc8c8
walwizard <goneisgone@hotmail.com>
2023-02-16 09:56:51 -0300

    Entendendo o BETWEEN

--- banco_de_dados_e_PHP/aula_11/index.php
+++ banco_de_dados_e_PHP/aula_11/index.php
@@ -1,6 +1,12 @@
 <?php
 $pdo = new PDO("mysql:hots=localhost;dbname=modulo_8", 'root', '99583100');
 
-$sql = $pdo->prepare("SELECT * FROM clientes WHERE nome LIKE '%g%'");
+$sql = $pdo->prepare("SELECT * FROM clientes  WHERE momento_registro BETWEEN '2023-02-14' AND '2023-02-15'");
 
 $sql->execute();
+
+$pessoas = $sql->fetchAll();
+
+foreach ($pessoas as $key => $value) {
+    echo $value['nome'];
+}
