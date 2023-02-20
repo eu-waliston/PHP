16297cfb77a831bb414c54a7e2539a73cfc3c6f6
walwizard <goneisgone@hotmail.com>
2023-02-20 09:45:31 -0300

    PHP e Bootstrap FIM projeto dashboard

---
+++ Bootstrpa_e_PHP/dashboard/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Painel de controle</title>
+    <!-- Bootstrap -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/syle.css">
+</head>
+
+<body>
+    <main>
+        <nav class="navbar navbar-expand-md navbar-default fixed-top bg-default">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Danki Code</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cadastrar equipe</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Editar Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Gerenciar Equipe</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a href="?sair" class="nav-link">Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </main>
+
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+
+</html>
