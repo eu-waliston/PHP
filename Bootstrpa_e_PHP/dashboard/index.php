c03d2c5b8f6d05e9ca82253140c76d0c60ba3929
walwizard <goneisgone@hotmail.com>
2023-02-20 10:07:43 -0300

    PHP e Bootstrap  projeto dashboard UI finished

--- Bootstrpa_e_PHP/dashboard/index.php
+++ Bootstrpa_e_PHP/dashboard/index.php
@@ -9,11 +9,12 @@
     <!-- Bootstrap -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/syle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 
 <body>
     <main>
-        <nav class="navbar navbar-expand-md navbar-default fixed-top bg-default">
+        <nav class="navbar navbar-expand-md navbar-default  bg-default">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Danki Code</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
@@ -34,13 +35,30 @@
                     </ul>
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a href="?sair" class="nav-link">Sair</a>
+                            <a href="?sair" class="nav-link"><i class="bi bi-box-arrow-left"></i> Sair</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </main>
+
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>
+                        <i class="bi bi-gear-fill"></i> Painel de controle
+                    </h2>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        <i class="bi bi-clock-fill"></i> seu ultimo login foi em 12/06/20023
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <script src="./js/bootstrap.min.js"></script>
 </body>
